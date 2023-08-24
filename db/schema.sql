274af09b7d702d6764656aaeb9cf72321a3b9ecf
Keeanu Rodriguez <keeanu7@gmail.com>
2023-08-23 21:30:45 -0400

    changing branch

--- db/schema.sql
+++ db/schema.sql
@@ -16,15 +16,15 @@ CREATE TABLE user (
     user_type TEXT,
 );
 
--- CREATE TABLE product (
---     product_id SERIAL PRIMARY KEY,
---     product_seller TEXT NOT NULL REFERENCES user (id),
---     product_name TEXT NOT NULL,
---     product_image TEXT NOT NULL,
---     product_cost DECIMAL(10,2) NOT NULL CHECK (product_cost >= 0),
---     product_weight DECIMAL(6,2) CHECK (product_weight >= 0),
---     product_unit_measurement TEXT,
---     product_category TEXT NOT NULL,
---     product_description TEXT,
---     product_stock INTEGER NOT NULL CHECK (product_stock >= 0),
--- );
+CREATE TABLE product (
+    product_id SERIAL PRIMARY KEY,
+    product_seller TEXT NOT NULL REFERENCES user (id),
+    product_name TEXT NOT NULL,
+    product_image TEXT NOT NULL,
+    product_cost DECIMAL(10,2) NOT NULL CHECK (product_cost >= 0),
+    product_weight DECIMAL(6,2) CHECK (product_weight >= 0),
+    product_unit_measurement TEXT,
+    product_category TEXT NOT NULL,
+    product_description TEXT,
+    product_stock INTEGER NOT NULL CHECK (product_stock >= 0),
+);
