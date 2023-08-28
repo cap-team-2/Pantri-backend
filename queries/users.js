7cb8de257ab881100753ae9262acd4e8bfadcd0e
joshuanelsondev <joshuanelsondev@gmail.com>
2023-08-28 19:21:19 -0400

    Update naming conventions and naming errors in usersController.js and users.js

--- queries/users.js
+++ queries/users.js
@@ -11,8 +11,8 @@ const getAllUsers = async () => {
 
 const getUser = async (id) => {
     try {
-        const oneUser = await db.oneOrNone("SELECT * FROM users WHERE id=$1", id);
-        return oneUser;
+        const user = await db.oneOrNone("SELECT * FROM users WHERE id=$1", id);
+        return user;
     } catch (error) {
         throw  error;
     };
@@ -24,6 +24,7 @@ const createUser = async () => {
             "INSERT INTO users(first_name, last_name, email, phone, address_1, address_2, city, zipcode, type) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
             [users.first_name, users.last_name, users.email, users.phone, users.address_1, users.address_2, users.city, users.zipcode, users.type]
         );
+        return newUser;
     } catch (error) {
         throw  error;
     };
@@ -35,6 +36,7 @@ const updateUser = async (id) => {
             "UPDATE users SET first_name=$1, last_name=$2, email=$3, phone=$4, address_1=$5, address_2=$5, city=$6, zipcode=$7, type=$8"
             [users.first_name, users.last_name, users.email, users.phone, users.address_1, users.address_2, users.city, users.zipcode, users.type]
         );
+        return updatedUser;
     } catch (error) {
         throw  error;
     };
