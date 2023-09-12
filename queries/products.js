09738a93d0452a794ff59b3a916fd62bf73c6f08
joshuanelsondev <joshuanelsondev@gmail.com>
2023-09-12 16:51:52 -0400

    Edit schema.sql, seed.sql, products.js, sellerProducts.js; update id to use uuid, seed data and table info names

--- queries/products.js
+++ queries/products.js
@@ -21,8 +21,8 @@ const getProduct = async (id) => {
 const createProduct = async () => {
     try {
         const newProduct = await db.one( 
-            "INSERT INTO products(seller, name, image, cost, weight, unit_measurement, category, description, stock) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
-            [products.seller, products.name, products.image, products.cost, products.weight, products.unit_measurement, products.category, products.description, products.stock]
+            "INSERT INTO products(id, seller_id, product_name, product_image, product_cost, product_weight, unit_measurement, category, product_description, stock) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *",
+            [products.id, products.seller_id, products.product_name, products.product_image, products.product_cost, products.product_weight, products.unit_measurement, products.category, products.product_description, products.stock]
         );
         return newProduct;
     } catch (error) {
@@ -33,8 +33,8 @@ const createProduct = async () => {
 const updateProduct = async (id) => {
     try {
         const updatedProduct = await db.one (
-            "UPDATE products SET seller=$1, name=$2, image=$3, cost=$4, weight=$5, unit_measurement=$6, category=$7, description=$8, stock=$9 WHERE id=$10 RETURNING *",
-            [products.seller, products.name, products.image, products.cost, products.weight, products.unit_measurement, products.category, products.description, products.stock, id]
+            "UPDATE products SET seller_id=$1, product_name=$2, product_image=$3, product_cost=$4, weight=$5, unit_measurement=$6, category=$7, product_description=$8, stock=$9 WHERE id=$10 RETURNING *",
+            [products.seller_id, products.product_name, products.product_image, products.product_cost, products.product_weight, products.unit_measurement, products.category, products.product_description, products.stock, id]
         );
         return updatedProduct;
     } catch (error) {
