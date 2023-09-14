69aa3d49606bd5fd2f0388c0dff87619e198970e
Keeanu Rodriguez <keeanu7@gmail.com>
2023-09-14 10:22:16 -0400

    small changes

--- controllers/search.js
+++ controllers/search.js
@@ -3,11 +3,10 @@ const search = express.Router();
 const { getAllProducts } = require("../queries/products.js")
 
 search.get("/:key",async (req, res) => {
-    const {key} = req.params
-    const products = await getAllProducts();
-    let data = await getAllProducts()
-    const data2 = data.filter((product) => product.name === key)
-    res.send(data2);
+    const {key} = req.params;
+    let data = await getAllProducts();
+    const filteredProducts = data.filter((product) => product.name.includes(key));
+    res.send(filteredProducts);
 })
 
 module.exports = search;
