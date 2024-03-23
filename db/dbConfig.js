fe956efc7cb786bbf5bcaac2e2bf028d16c65154
joshuanelsondev <joshuanelsondev@gmail.com>
2024-03-23 14:41:25 -0400

    Update dbConfig file

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -4,8 +4,10 @@ require("dotenv").config();
 const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
 
 const db = pgp({
-  connectionString: DATABASE_URL || `postgres://${PG_USER}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`,
+  connectionString: DATABASE_URL 
 });
+
+
 
 module.exports = db;
 
