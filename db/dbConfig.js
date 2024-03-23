df3775644fb2f8f9dec466c3c762e7c6daefb199
joshuanelsondev <joshuanelsondev@gmail.com>
2024-03-23 12:07:24 -0400

    Update .env and dbConfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,22 +1,9 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+const { DATABASE_URL } = process.env;
 
-const cn = DATABASE_URL
-?{
-    connectionString: DATABASE_URL,
-    max: 30,
-}
-: {
-    host: PG_HOST || "localhost",
-    port: PG_PORT,
-    database: PG_DATABASE,
-    user: PG_USER,
-};
-console.log(process.env.DATABASE_URL);
-console.log(process.env.PG_HOST);
-const db = pgp(cn);
+const db = pgp(DATABASE_URL);
 
 module.exports = db;
 
