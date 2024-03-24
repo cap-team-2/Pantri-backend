f8d6cd3bf01f3d08e522de585332e98f28244c12
joshuanelsondev <joshuanelsondev@gmail.com>
2024-03-23 23:57:10 -0400

    db

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,21 +1,31 @@
-const pgp = require('pg-promise')();
-require('dotenv').config();
+// const pgp = require('pg-promise')();
+// require('dotenv').config();
 
-const { DATABASE_URL } = process.env;
+// const { DATABASE_URL } = process.env;
 
-const cn = DATABASE_URL
-  ? {
-      connectionString: DATABASE_URL,
-      max: 30,
-    }
-  : {
-      host: process.env.PG_HOST,
-      port: process.env.PG_PORT,
-      database: process.env.PG_DATABASE,
-      user: process.env.PG_USER,
-    };
+// const cn = DATABASE_URL
+//   ? {
+//       connectionString: DATABASE_URL,
+//       max: 30,
+//     }
+//   : {
+//       host: process.env.PG_HOST,
+//       port: process.env.PG_PORT,
+//       database: process.env.PG_DATABASE,
+//       user: process.env.PG_USER,
+//     };
 
-const db = pgp(cn);
+// const db = pgp(cn);
+
+const pgp = require("pg-promise")();
+
+const db = pgp({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = db;
+
 
