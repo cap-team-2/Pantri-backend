3f45f3842563d6202e71e1a1d817b119ad3f8142
joshuanelsondev <joshuanelsondev@gmail.com>
2024-03-25 22:27:48 -0400

    Update dbConfig

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,7 +1,13 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const db = pgp(process.env.DATABASE_URL);
+const db = pgp({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 
 
 module.exports = db;
